08cea3c82d8ddc7c6a001995d2227b260b7a8685
Girish21 <girishkumarv21@gmail.com>
2022-06-19 16:46:13 +0530

    login action handler

--- packages/remix-app/app/components/auth-form.tsx
+++ packages/remix-app/app/components/auth-form.tsx
@@ -8,7 +8,7 @@ export function AuthForm() {
 
   return (
     <Form method='post'>
-      <fieldset className='flex w-[min(30ch,100vw-2rem)] flex-col gap-1'>
+      <fieldset className='flex w-[min(45ch,100vw-2rem)] flex-col gap-4'>
         <label
           htmlFor='user-name'
           className='block text-lg text-gray-900 dark:text-slate-100'
@@ -19,7 +19,7 @@ export function AuthForm() {
           id='user-name'
           name='userName'
           className={clsx(
-            'w-full border-b-2 border-b-gray-900 bg-transparent p-2 pl-0 text-lg text-gray-900 focus:outline-none focus-visible:border-b-blue-500 dark:border-b-slate-100 dark:text-slate-100',
+            'w-full border-b-2 border-b-gray-900 bg-transparent pb-2 pr-2 text-lg text-gray-900 focus:outline-none focus-visible:border-b-blue-500 dark:border-b-slate-100 dark:text-slate-100',
             error && 'border-b-red-500',
           )}
           autoComplete='off'
@@ -28,7 +28,7 @@ export function AuthForm() {
         />
         {error && (
           <p
-            className='bg-red-400 p-2 text-base font-bold text-gray-900 ring-2 ring-red-700 ring-offset-2 ring-offset-transparent'
+            className='rounded bg-red-400 p-2 text-base font-bold text-gray-900 ring-2 ring-red-500 ring-offset-2 ring-offset-transparent dark:ring-red-700'
             id='user-name-error'
           >
             {error}
--- packages/remix-app/app/route-containers/login/login.server.ts
+++ packages/remix-app/app/route-containers/login/login.server.ts
@@ -1,6 +1,41 @@
 import type { ActionFunction } from '@remix-run/cloudflare'
-import { json } from '@remix-run/cloudflare'
+import { json, redirect } from '@remix-run/cloudflare'
+import * as z from 'zod'
+import { commitSession, getSession } from '~/session/user.server'
+import { validateName } from '~/utils'
 
-export let action: ActionFunction = async () => {
-  return json(null)
+export let action: ActionFunction = async ({ context, request }) => {
+  let [session, formData] = await Promise.all([
+    getSession(request, context.env),
+    request.formData(),
+  ])
+
+  let name: string | null
+  try {
+    name = validateName(formData.get('userName'))
+  } catch (e) {
+    if (e instanceof z.ZodError) {
+      return json<ActionData>({ error: e.issues.at(0)?.message })
+    }
+    return json<ActionData>({ error: 'Unknown error' })
+  }
+
+  let kvUser = await context.env.USER.get(name)
+
+  if (!kvUser) {
+    throw redirect(
+      `/signup?${new URLSearchParams([
+        ['userName', name],
+        ['error', 'User not found'],
+      ])}`,
+    )
+  }
+
+  session.set('userName', name)
+
+  return redirect('/', {
+    headers: { 'Set-Cookie': await commitSession(session, context.env) },
+  })
 }
+
+type ActionData = { error: string | undefined } | null
